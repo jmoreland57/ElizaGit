2b9223f9936521e31e1727c4bfb89e9aa71d9297
Eliza Koblentz <ekoblentz1@hwemail.com>
2022-09-22 16:04:44 -0700

    Writing To File Works

--- src/Commit.java
+++ src/Commit.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -28,7 +29,7 @@ public LocalDate d;
 private Commit par;
 private Commit oth;
 
-	public Commit(String summary, String author, String pTree, Commit parent) throws IOException
+	public Commit(String summary, String author, String pTree, Commit parent) throws IOException, NoSuchAlgorithmException
 	{
 		this.pTree = pTree;
 		sum = summary;
@@ -40,6 +41,16 @@ private Commit oth;
 		oth = null;
 		
 		writeToFile();
+		/*
+		if (par !=null || oth != null)
+		{
+			String sha = par.generateSha1(par.getContentOfFile());
+			System.out.println (sha);
+			readFile(sha);
+			
+		}
+		*/	
+		
 	}
 	
 	public LocalDate getDate()
@@ -47,26 +58,49 @@ private Commit oth;
 		LocalDate d = java.time.LocalDate.now();
 		return d;
 	}
-	
+	 public String generateSha1(String input)
+	            throws NoSuchAlgorithmException, UnsupportedEncodingException
+	        {
+	            MessageDigest md = MessageDigest.getInstance("SHA1");
+	            md.reset();
+	            byte[] buffer = input.getBytes("UTF-8");
+	            md.update(buffer);
+	            byte[] digest = md.digest();
+
+	            String hexStr = "";
+	            for (int i = 0; i < digest.length; i++) {
+	                hexStr +=  Integer.toString( ( digest[i] & 0xff ) + 0x100, 16).substring( 1 );
+	            }
+	            return hexStr;
+	        }
+	/*
 	public String generateSha1(String input)
 	{
-		String sha1 = "";
-	    try
-	    {
-	        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
-	        crypt.reset();
-	        crypt.update(input.getBytes("UTF-8"));
-	        sha1 = byteToHex(crypt.digest());
-	    }
-	    catch(NoSuchAlgorithmException e)
-	    {
-	        e.printStackTrace();
-	    }
-	    catch(UnsupportedEncodingException e)
-	    {
-	        e.printStackTrace();
-	    }
-	    return sha1;
+		if (input == null)
+		{
+			String sha1 = "";
+		    try
+		    {
+		        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+		        crypt.reset();
+		        crypt.update(input.getBytes("UTF-8"));
+		        sha1 = byteToHex(crypt.digest());
+		    }
+		    catch(NoSuchAlgorithmException e)
+		    {
+		        e.printStackTrace();
+		    }
+		    catch(UnsupportedEncodingException e)
+		    {
+		        e.printStackTrace();
+		    }
+		    return sha1;
+		}
+		else
+		{
+			return "";
+		}
+		
 	}
 
 	private static String byteToHex(final byte[] hash)
@@ -80,11 +114,11 @@ private Commit oth;
 	    formatter.close();
 	    return result;
 	}
-	
-	public String getContentOfFile()
+*/
+	public String getContentOfFile() throws NoSuchAlgorithmException, UnsupportedEncodingException
 	{
 		String content = "";
-		content += ("objects/" + pTree);
+		content += ("objects/" + generateSha1(pTree));
 		if (par != null)
 		{
 			content += ("\nobjects/" + par.generateSha1(par.getContentOfFile()) + "\n");
@@ -93,7 +127,7 @@ private Commit oth;
 		{
 			content += "\n";
 		}
-		
+			
 		if (oth != null)
 		{
 			content += ("objects/" + oth.generateSha1(par.getContentOfFile()) + "\n");
@@ -102,72 +136,37 @@ private Commit oth;
 		{
 			content += "\n";
 		}
-		
+			
 		content += auth+"\n";
 		content += d +"\n";
 		content +=sum + "\n";
-		content += "This commit is amazing!";
-		
+
 		return content;
+		
 	}
-	public void writeToFile()
+	
+	public void writeToFile() throws NoSuchAlgorithmException, UnsupportedEncodingException
 	{
-        Path p = Paths.get("objects/" + generateSha1(getContentOfFile()));
+		String sha = generateSha1(getContentOfFile());
+		//System.out.println ("this is content" + sha);
+        Path p = Paths.get(sha);
+       // System.out.println (p.toAbsolutePath());
+        
         try {
-            Files.writeString(p, "example", StandardCharsets.ISO_8859_1);
+            Files.writeString(p, getContentOfFile(), StandardCharsets.ISO_8859_1);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         
 	}
-		 
-		 /*
-		  * read a file to a string
-		  * replace the third line of the string with ""
-		  * put that string into a file
-		  */
-	
-	public String readFile (String fileName) throws IOException
+	/*		
+	public void changeParent() throws IOException
 	{
-		Path filePath = Paths.get(fileName);
-		String content = Files.readString(filePath);
-		System.out.println (content);
-		return content;
-	}
-	
-	public String replaceThirdLine (String content)
-	{
-		int first = 0;
-		int second = content.indexOf("\n");
-		String temp = "";
-		for (int pos = content.indexOf("\n"); pos != -1; pos = content.indexOf("\n", pos + 1)) 
-		{
-			  second = pos;
-			  if (content.substring(first, second).equals("\n"+auth))
-			  {
-				  temp += "\n";
-				  first = second;
-			  }
-			  else
-			  {
-				  temp += content.substring(first, second);
-				  first = second;
-			  }
-		}
-		content = temp;
-		return content;
-	}
-	
-	public String putInFile (String newContent)
-	{
-		return "hey";
-	}
-		/*
 		if (par != null)
 		{
-			String filePath = "objects/" + generateSha1(getContentOfFile());
-			setVariable(3, filePath, "objects/" + par.generateSha1(par.getContentOfFile()));
+			String spot = "objects/" + generateSha1(getContentOfFile());
+			setVariable(3, spot, ("objects/" + par.generateSha1(par.getContentOfFile())));
 		}
 		else
 		{
@@ -175,24 +174,100 @@ private Commit oth;
 		}
 	}
 	
-	public static void setVariable(int lineNumber, String data, String fileName) throws IOException 
-	{
-	    Path path = Paths.get(fileName);
+	public static void setVariable(int lineNumber, String data, String nameOfFile) throws IOException {
+	    Path path = Paths.get(nameOfFile);
 	    ArrayList<String> lines = (ArrayList<String>)Files.readAllLines(path, StandardCharsets.UTF_8);
-	    //lines.set(lineNumber-1, data);
+	    lines.set(lineNumber-3, data);
 	    Files.write(path, lines, StandardCharsets.UTF_8);
 	}
-	*/
+		*/ 
+		 /*
+		  * read a file to a string
+		  * replace the third line of the string with ""
+		  * put that string into a file
+		  */
+	
 
-	public static void main (String [] args) throws IOException
+	public void readFile (String fileName) throws IOException, NoSuchAlgorithmException
 	{
-		Commit test1 = new Commit ("summary", "author", "Eliza Koblentz", null);
-		Commit test2 = new Commit ("summary", "author", "Amelia Koblentz", test1);
+		if (par != null) {
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			String content = "";
+			
+			content += reader.readLine() + "/n";
+			content += reader.readLine() + "/n";
+			content += generateSha1(getContentOfFile());
+			content += reader.readLine() + "/n";
+			content += reader.readLine() + "/n";
+			content += reader.readLine() + "/n";
+			
+			File file = new File ("./objects/" + fileName);
+			PrintWriter print = new PrintWriter (file);
+			print.print(content);
+			print.close();
+		}
+		else
+		{
+			return;
+		}
 		
+		/*
+		Path filePath = Paths.get(fileName);
+		String content = Files.readString(filePath);
+		System.out.println (content);
+		return content;
+		*/
+	}
+	/*
+	public String replaceThirdLine (String content)
+	{
+		if (par != null) 
+		{
+			int first = 0;
+			int second = content.indexOf("\n");
+			String temp = "";
+			for (int pos = content.indexOf("\n"); pos != -1; pos = content.indexOf("\n", pos + 1)) 
+			{
+				  second = pos;
+				  if (content.substring(first, second).equals("\n"))
+				  {
+					  temp += "\n";
+					  first = second;
+				  }
+				  else
+				  {
+					  temp += content.substring(first, second);
+					  first = second;
+				  }
+			}
+			content = temp;
+		}
+		
+		return content;
+	}
+	
+	public void putInFile (String newContent)
+	{
+		 Path p = Paths.get("./objects/temporary.txt");
+	        try {
+	            Files.writeString(p, replaceThirdLine(readFile("temporary.txt")), StandardCharsets.ISO_8859_1);
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+
+	}
+*/
+	public static void main (String [] args) throws IOException, NoSuchAlgorithmException
+	{
+		Commit test1 = new Commit ("summary1", "author1", "Eliza Koblentz", null);
+		System.out.println ("Test 1 content:\n" + test1.getContentOfFile());
+		//Commit test2 = new Commit ("summary2", "author2", "Amelia Koblentz", test1);
+		//System.out.println ("Test 2 content:\n" + test2.getContentOfFile());
 		//test1.readFile("temporary.txt");
-		System.out.println (test2.getContentOfFile());
+		//System.out.println (test1.getContentOfFile());
 		//String read = "Hey there\nMy name is Eliza\nI am a computer\n";
 		//System.out.println(test1.replaceThirdLine(read));
 	}
-	
+	 
 }
